d9cbdbc4b7a9aaa29bf263c13197ca7a55b3f7b1
ajaypandey <ajaypandey8322@gmail.com>
2024-03-18 21:57:16 +0530

    adding an updated version

---
+++ Main.java
@@ -0,0 +1,18 @@
+package javaProjectJenkins;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a java program");
+		
+		System.out.println("******");
+		System.out.println(new Date());
+		System.out.println("******");
+		System.out.println("lets see after update if it works !!!!!!!!!!!!!);
+		
+	}
+
+}
