35d6f134078cebb5b35c6b5098fc20ef02ccd251
Ajay Kumar Pandey <85065514+ajaypandey8322@users.noreply.github.com>
2024-03-18 15:23:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package javaProjectJenkins;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a java program");
+		
+		System.out.println("******");
+		System.out.println(new Date());
+		System.out.println("******");
+	}
+
+}
